7e3311091fa409f280ecce167f774c6544a79451
Shubhanshi <shubhanshiofficial12@gmail.com>
2025-06-11 10:37:27 +0530

    Added login auth, search bar and add patient to the list

--- pams/urls.py
+++ pams/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.patient_list, name='patient_list'),
+    path('add/', views.add_patient, name='add_patient'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- patient_management/settings.py
+++ patient_management/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'patient_list'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
